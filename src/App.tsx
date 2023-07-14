623ece9df94b6f1e3a247b397423156f7f13893c
eridervishi <eri.dervishi@bit.al>
2023-07-14 16:31:52 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { ConfigProvider, theme } from "antd";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { AuthProvider } from "./contexts/AuthContext";
+
+import { store, persistor } from "./store/redux";
+
+import Router from "./router";
+
+import Loading from "./components/Loading/Loading";
+
+const themeConfig = {
+  algorithm: theme.defaultAlgorithm,
+  token: {
+    colorPrimary: "#3d7cef",
+  },
+};
+
+const reactQueryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <HelmetProvider>
+      <ReduxProvider store={store}>
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <AuthProvider>
+            <QueryClientProvider client={reactQueryClient}>
+              <ConfigProvider theme={themeConfig}>
+                <BrowserRouter>
+                  <Router />
+                </BrowserRouter>
+              </ConfigProvider>
+            </QueryClientProvider>
+          </AuthProvider>
+        </PersistGate>
+      </ReduxProvider>
+    </HelmetProvider>
+  );
+};
+
+export default App;
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,67 @@
+import {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+
+import { User } from "../types/user";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+
+  authenticateUser: (user: Partial<User>) => void;
+  unAuthenticateUser: () => void;
+  updateUser: () => void;
+}
+
+const initialState: AuthContextType = {
+  isAuthenticated: false,
+
+  authenticateUser: () => {},
+  unAuthenticateUser: () => {},
+  updateUser: () => {},
+};
+
+const AuthContext = createContext(initialState);
+
+const AuthProvider: FunctionComponent<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const isAuthenticated = useMemo<boolean>(() => Boolean(user), [user]);
+
+  const authenticateUser = (user: Partial<User>) => {
+    //
+  };
+
+  const unAuthenticateUser = () => {
+    setUser(null);
+  };
+
+  const updateUser = () => {};
+
+  useEffect(() => {
+    updateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...initialState,
+        isAuthenticated,
+        authenticateUser,
+        unAuthenticateUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
